8dd93e05b9a832439194c190bc2fb605f2fa5849
Vladimir Kochnev <v.kochnev@1tvch.ru>
2014-09-15 12:09:12 +0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TryTsvectorInRails31::Application.config.secret_token = 'aa1148d9e700875337c1cb022348118e1ac21b65b7947655db22a5317da2b5ce930beebce9ba033f1f1a18994f0463839b8992f3b6a84922e732ad2f41416027'
